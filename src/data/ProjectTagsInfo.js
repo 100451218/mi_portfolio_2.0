949a65c11c459b3f878646e3806a792431fce3f8
_100451218 <98971345+100451218@users.noreply.github.com>
2025-12-04 10:21:27 +0000

    Separated the creation of tags and added images

---
+++ src/data/ProjectTagsInfo.js
@@ -0,0 +1,35 @@
+// ProjectTagsInfo.js
+// Diccionario centralizado de tags con icono (imagen) y nombre
+
+const projectTags = {
+  react: {
+    name: "React",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+  },
+  typescript: {
+    name: "TypeScript",
+    icon: "/public/icons/typescript.png",
+  },
+  javascript: {
+    name: "JavaScript",
+    icon: "/public/icons/javascript.png",
+  },
+  css: {
+    name: "CSS",
+    icon: "/public/icons/css.png",
+  },
+  html: {
+    name: "HTML",
+    icon: "/public/icons/html.png",
+  },
+  ux: {
+    name: "UX Design",
+    icon: "/public/icons/ux.png",
+  },
+  placeholder: {
+    name: "Tag_Placeholder",
+    icon: "/public/placeholder.png",
+  },
+};
+
+export default projectTags;
